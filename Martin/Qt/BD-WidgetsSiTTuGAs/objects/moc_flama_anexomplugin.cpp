99cec50929a7fb7f5ba58bf248bcc05a01c5504f
Horacio Chávez Carrillo <horchazca@gmail.com>
2013-12-09 19:22:53 -0600

    Compilacion Qt

--- Martin/Qt/BD-WidgetsSiTTuGAs/objects/moc_flama_anexomplugin.cpp
+++ Martin/Qt/BD-WidgetsSiTTuGAs/objects/moc_flama_anexomplugin.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'flama_anexomplugin.h'
 **
-** Created: Sat 7. Dec 00:52:52 2013
+** Created: Mon 9. Dec 19:17:49 2013
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
 **
 ** WARNING! All changes made in this file will be lost!
